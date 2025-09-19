497bd20b9fa265cacd321dd351d7d8b9efacc6e9
OlliK-dev <oltsuuka@gmail.com>
2025-09-19 12:47:00 +0300

    fixed the counter button

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 'use strict';
 
+import { initCounterButton } from './scripts/counter.js';
 import { $ } from './scripts/helpers.js'
 import { initThemeButton } from './scripts/theme.js';
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
     initThemeButton()
+    initCounterButton()
 })
--- scripts/counter.js
+++ scripts/counter.js
@@ -1,9 +1,14 @@
 import { $ } from './helpers.js'
 
 // 3) Laskuri — virhe: event delegation ja bubbling sekoilee
-const counterBtn = $('.counter');
-counterBtn.addEventListener('click', (e) => {
-    if (e.target.classList.contains('count')) return; // BUG: estää klikin
-    const span = $('.count', counterBtn);
-    span.textContent = String(parseInt(span.textContent, 10) + 1);
-});
+
+
+
+export function initCounterButton(){
+    const counterBtn = $('.counter');
+    counterBtn.addEventListener('click', (e) => {
+        if (e.target.classList.contains('.count')) return;
+        const span = $('.count', counterBtn);
+        span.textContent = String(parseInt(span.textContent, 10) + 1);
+    }); 
+}
